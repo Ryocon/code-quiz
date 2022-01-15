b6858dc6333abe3ca3dca55901bf55cbf513faaf
roconn <roconn25@gmail.com>
2022-01-15 21:34:33 +0000

    swapped script names

--- js/script.js
+++ js/script.js
@@ -1,183 +1,152 @@
-var startButton = document.getElementById('start-button')
-var quizContainer = document.getElementById('quiz')
+
+// variable containing an array of objects for the questions
+var quizQuestions = [
+    {
+      question: "What is 1+1",
+      answers: ["2", "3", "q", "egg", "E"],
+      correctAnswer: "2",
+    },
+    {
+      question: " Q 2",
+      answers: ["A", "B", "C", "D", "E"],
+      correctAnswer: "B",
+    },
+    {
+      question: "Question 3",
+      answers: ["A", "B", "C", "D", "E"],
+      correctAnswer: "C",
+    },
+    {
+      question: " Q 4",
+      answers: ["A", "B", "C", "D", "E"],
+      correctAnswer: "D",
+    },
+  ];
+
+
+
+var questionIndex = 0
+
 var questionEl = document.getElementById('question')
-var answerEl = document.getElementById('answers')
-var mainSplash = document.getElementById('main')
-var container = document.getElementsByClassName('container')
+
+var answersEl = document.getElementById('answers')
+
+var mainEl = document.getElementById('main')
+var startButton = document.getElementById('start-button')
+var quizEl = document.getElementById('quiz')
 var timerEl = document.getElementById('timer')
-var endPage = document.getElementById('end-page')
-// for moving to the next question set
-var nextQuestion = document.getElementById('next')
-// undefined arrays to allow questions
-var questionRandom, questionIndex
+var endPageEl = document.getElementById('end-page')
+var backButton = document.getElementById('back-button')
 
-// variables to work out high score
-var correctAnswers = 0
-var falseAnswers = 0
+// timer initial setting in global scope
+var timeLeft = 10
+var wrongAnswer = 3
 
-var highscoreStorage
+// user starting score
+var userScore = document.getElementById('score')
+var userScore = 0
 
-// declared at global scope to be accessed elsewhere
-var timeLeft = 11
-
-
-
+// event listener that calls the start startQuiz function
 startButton.addEventListener('click', startQuiz)
-
-// debugger
 
 function startQuiz() {
  console.log('Started')
- mainSplash.classList.add('hide')
- questionRandom = questions.sort(() => Math.random() - .5)
- questionIndex = 0
- quizContainer.classList.remove('hide')
+ mainEl.classList.add('hide')
+ quizEl.classList.remove('hide')
+ renderQuestion()
  countDown()
-setQuestion()
-correctAnswers = 0
-falseAnswers = 0
 }
 
-// hides and removes elements when time is up
 function stopQuiz() {
     clearInterval(timerEl)
-    quizContainer.classList.add('hide')
-    endPage.classList.remove('hide')
+    quizEl.classList.add('hide')
+    endPageEl.classList.remove('hide')
     timerEl.innerHTML = ' is up!'
-    renderHighScores()
+    displayScore()
+    clearTimeout(countDown, 1000)
 }
 
-// countdown function using setinterval
+function displayScore() {
+    debugger
+    userScore.textContent = userScore
+    console.log('Your final score is ' + userScore)
+    
+}
+
 function countDown() {
-    var timeLeft = 11
+    // var timeLeft = 11
     quizTimer = setInterval(function(){
         timeLeft--
         timerEl.innerText = timeLeft
 if (timeLeft <= 0) {
-    // clearInterval(timerEl)
+    clearInterval(timerEl)
     stopQuiz()
     // quizContainer.classList.add('hide')
     // endPage.classList.remove('hide')
     // timerEl.innerHTML = ' is up!'
 }
 // DOES THIS WORK WHEN END OF QUESTIONS?
-if (questionIndex >= questions.length) {
-    clearInterval(timer);
-}
+// if (questionIndex >= questions.length) {
+//     clearInterval(timer);
+// }
 }, 1000 ) }
 
-function setQuestion() {
-    showQuestion(questionRandom[questionIndex])
+
+function renderQuestion() {
+    var questionObject = quizQuestions[questionIndex]
+
+    questionEl.textContent = questionObject.question
+
+    answersEl.innerHTML = ""
+
+    questionObject.answers.forEach(function(answer){
+       var answerButton = document.createElement('button')
+       answerButton.setAttribute('value', answer)
+
+       answerButton.textContent = answer
+
+       answerButton.onclick = answerChoice
+       answersEl.appendChild(answerButton)
+    }
+  ) 
 }
 
-// foreach executes a provided function once for each array element
-function showQuestion(question) {
-    questionEl.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('button-style')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        // Uncaught TypeError: answerEl.appendChild is not a function
-        answerEl.appendChild(button)
-    })
-}
 
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerEl.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (questionRandom.length > questionIndex + 1) {
-  
-} }
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
+function answerChoice() {
+    if (this.value !== quizQuestions[questionIndex].correctAnswer) {
+        console.log('wrong')
+        timeLeft = timeLeft - wrongAnswer
     } else {
-      element.classList.add('wrong')
+        console.log('correct')
+        userScore++
+        console.log(userScore)
+        questionIndex++
+        // add to user score
     }
-  }
 
-  function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-  }
 
-// WOKRING ON QUESTIONS APPEARING
-// correct answers will need to increment in order to set highscore
-
-function resetPage() {
-    while (answerEl.firstChild) {
-        answerEl.removeChild
-        (answerEl.firstChild)
+    if (questionIndex === quizQuestions.length) {
+        stopQuiz()
+        clearInterval(countDown)
+    } else {
+        renderQuestion()
     }
 }
 
 
+// local storage HELL
 
 
-
-
-function subtractTime()  {
-
+function storeHighscore() {
+    
 }
 
-// function renderHighScores() {
-//     // removepreviouscontent
-
-//     var highscores = localStorage.getItem("highscores")
-
-//     // this needs major work
-//     if (highscores === null) {
-//         var parsedHighscores = JSON.parse(highscores)
-
-// for (var i = 0; i < parsedHighscores.length; i++) {
-//     var highscore = parsedHighscores[1]
-
-
-//     var score = highscore.score
-//     var initials = highscore.initials
-// }
-
-//         // obtain user initials via input field (use .val)
-
-//         // localStorage.setItem("highscores")
-//     }
-
-//     localStorage.setItem("highscores", )
-// }
-
-// create input field for user initials
-// create event listener for user input to write to local storage for highscores
+var highScores = JSON.parse(localStorage.getItem('highScores'))
 
 
 
+backButton.addEventListener('click', goBack)
 
-const questions = [
-    {
-        question: 'What is an Egg?',
-        answers: [
-            { text: 'Round', correct: false},
-            { text: 'Bird', correct: false },
-            { text: 'Food', correct: true},
-            { text: 'Java', correct: false}
-        ]
-    },
-    {
-        question: 'test',
-        answers: [
-            {text: 'test', correct: true},
-            {text: 'test', correct: false},
-            {text: 'test', correct: false},
-            {text: 'test', correct: false}
-        ]
-    }
-]
+function goBack () {
+location.reload();
+}
--- js/script2.js
+++ js/script2.js
@@ -1,64 +1,55 @@
-
-// variable containing an array of objects for the questions
-var quizQuestions = [
-    {
-      question: "What is 1+1",
-      answers: ["2", "3", "q", "egg", "E"],
-      correctAnswer: "2",
-    },
-    {
-      question: " Q 2",
-      answers: ["A", "B", "C", "D", "E"],
-      correctAnswer: "B",
-    },
-    {
-      question: "Question 3",
-      answers: ["A", "B", "C", "D", "E"],
-      correctAnswer: "C",
-    },
-    {
-      question: " Q 4",
-      answers: ["A", "B", "C", "D", "E"],
-      correctAnswer: "D",
-    },
-  ];
-
-var questionIndex = 0
-
-var questionEl = document.getElementById('question')
-
-var answersEl = document.getElementById('answers')
-
-var mainEl = document.getElementById('main')
 var startButton = document.getElementById('start-button')
-var quizEl = document.getElementById('quiz')
+var quizContainer = document.getElementById('quiz')
+var questionEl = document.getElementById('question')
+var answerEl = document.getElementById('answers')
+var mainSplash = document.getElementById('main')
+var container = document.getElementsByClassName('container')
 var timerEl = document.getElementById('timer')
-var endPageEl = document.getElementById('end-page')
+var endPage = document.getElementById('end-page')
+// for moving to the next question set
+var nextQuestion = document.getElementById('next')
+// undefined arrays to allow questions
+var questionRandom, questionIndex
 
-// timer initial setting in global scope
-var timeLeft = 10
+// variables to work out high score
+var correctAnswers = 0
+var falseAnswers = 0
 
-// event listener that calls the start startQuiz function
+var highscoreStorage
+
+// declared at global scope to be accessed elsewhere
+var timeLeft = 11
+
+
+
 startButton.addEventListener('click', startQuiz)
+
+// debugger
 
 function startQuiz() {
  console.log('Started')
- mainEl.classList.add('hide')
- quizEl.classList.remove('hide')
- renderQuestion()
+ mainSplash.classList.add('hide')
+ questionRandom = questions.sort(() => Math.random() - .5)
+ questionIndex = 0
+ quizContainer.classList.remove('hide')
  countDown()
+setQuestion()
+correctAnswers = 0
+falseAnswers = 0
 }
 
-
+// hides and removes elements when time is up
 function stopQuiz() {
     clearInterval(timerEl)
-    quizEl.classList.add('hide')
-    endPageEl.classList.remove('hide')
+    quizContainer.classList.add('hide')
+    endPage.classList.remove('hide')
     timerEl.innerHTML = ' is up!'
+    renderHighScores()
 }
 
+// countdown function using setinterval
 function countDown() {
-    // var timeLeft = 11
+    var timeLeft = 11
     quizTimer = setInterval(function(){
         timeLeft--
         timerEl.innerText = timeLeft
@@ -70,49 +61,123 @@ if (timeLeft <= 0) {
     // timerEl.innerHTML = ' is up!'
 }
 // DOES THIS WORK WHEN END OF QUESTIONS?
-// if (questionIndex >= questions.length) {
-//     clearInterval(timer);
-// }
+if (questionIndex >= questions.length) {
+    clearInterval(timer);
+}
 }, 1000 ) }
 
-function setQuestion() {}
+function setQuestion() {
+    showQuestion(questionRandom[questionIndex])
+}
 
-function renderQuestion() {
-    var questionObject = quizQuestions[questionIndex]
+// foreach executes a provided function once for each array element
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('button-style')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        // Uncaught TypeError: answerEl.appendChild is not a function
+        answerEl.appendChild(button)
+    })
+}
 
-    questionEl.textContent = questionObject.question
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (questionRandom.length > questionIndex + 1) {
+  
+} }
 
-    answersEl.innerHTML = ""
-
-    questionObject.answers.forEach(function(answer){
-       var answerButton = document.createElement('button')
-       answerButton.setAttribute('value', answer)
-
-       answerButton.textContent = answer
-
-       answerButton.onclick = answerChoice
-       answersEl.appendChild(answerButton)
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
     }
-  ) 
+  }
+
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
+// WOKRING ON QUESTIONS APPEARING
+// correct answers will need to increment in order to set highscore
+
+function resetPage() {
+    while (answerEl.firstChild) {
+        answerEl.removeChild
+        (answerEl.firstChild)
+    }
 }
 
 
-function answerChoice() {
-    if (this.value !== quizQuestions[questionIndex].correctAnswer) {
-        console.log('wrong')
-    } else {
-        console.log('correct')
-        questionIndex++
-        // add to user score
-    }
 
 
-    if (questionIndex === quizQuestions.length) {
-        stopQuiz()
 
-        clearInterval(countDown)
-    } else {
-        renderQuestion()
-    }
+
+function subtractTime()  {
+
 }
 
+// function renderHighScores() {
+//     // removepreviouscontent
+
+//     var highscores = localStorage.getItem("highscores")
+
+//     // this needs major work
+//     if (highscores === null) {
+//         var parsedHighscores = JSON.parse(highscores)
+
+// for (var i = 0; i < parsedHighscores.length; i++) {
+//     var highscore = parsedHighscores[1]
+
+
+//     var score = highscore.score
+//     var initials = highscore.initials
+// }
+
+//         // obtain user initials via input field (use .val)
+
+//         // localStorage.setItem("highscores")
+//     }
+
+//     localStorage.setItem("highscores", )
+// }
+
+// create input field for user initials
+// create event listener for user input to write to local storage for highscores
+
+
+
+
+const questions = [
+    {
+        question: 'What is an Egg?',
+        answers: [
+            { text: 'Round', correct: false},
+            { text: 'Bird', correct: false },
+            { text: 'Food', correct: true},
+            { text: 'Java', correct: false}
+        ]
+    },
+    {
+        question: 'test',
+        answers: [
+            {text: 'test', correct: true},
+            {text: 'test', correct: false},
+            {text: 'test', correct: false},
+            {text: 'test', correct: false}
+        ]
+    }
+]
